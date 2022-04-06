e4cf5a46af3c3bd3597033196bcc8c025750a3a5
benharlucas <benhar.masahod@msugensan.edu.ph>
2022-04-06 10:32:39 +0800

    Second Commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = () => {
+    const home = 'Home';
+    const aboutUs = 'About us'
+
+    return (
+        <header>
+            <p>{home} {about us}</p>
+        </header>
+    )
+        
+};
+
+export default Header
